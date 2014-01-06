8d9d7660de10a0806169cbc91ffe93314985f845
Evgeniy Karkan <Evgeniy.Karkan@sigmaukraine.com>
2014-01-06 11:23:21 +0200

    Added new project >> code & autolayout demo.

---
+++ AutoLayoutsViaCode&Constraints/AutoLayoutsViaCode&ConstraintsTests/AutoLayoutsViaCode_ConstraintsTests.m
@@ -0,0 +1,34 @@
+//
+//  AutoLayoutsViaCode_ConstraintsTests.m
+//  AutoLayoutsViaCode&ConstraintsTests
+//
+//  Created by Evgeniy Karkan on 1/6/14.
+//  Copyright (c) 2014 EvgenyKarkan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutoLayoutsViaCode_ConstraintsTests : XCTestCase
+
+@end
+
+@implementation AutoLayoutsViaCode_ConstraintsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
